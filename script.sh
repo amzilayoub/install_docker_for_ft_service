54b493a384149c6b0a4c71e701ec8918836c37de
Ayoub Amzil <aamzil@e2r12p2.1337.ma>
2020-10-19 20:02:05 +0100

    first commit

---
+++ script.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+mkdir /goinfre/$1/.brew && curl -fsSL https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C /goinfre/$1/.brew
+mkdir -p /tmp/.$(whoami)-brew-locks
+mkdir -p /goinfre/$1/.brew/var/homebrew
+ln -s /tmp/.$(whoami)-brew-locks /goinfre/$1/.brew/var/homebrew/locks
+echo 'export PATH="/goinfre/$1/.brew/bin:$PATH"' >> ~/.bashrc
+echo 'export PATH="/goinfre/$1/.brew/bin:$PATH"' >> ~/.zshrc
+ln -s /goinfre/$1/.brew ~/.brown
+brew update && brew upgrade
+mkdir -p /tmp/.$(whoami)-brew-locks
+echo 'export PATH="$HOME/.brown/bin:$PATH"' >> ~/.bashrc
+echo 'export PATH="$HOME/.brown/bin:$PATH"' >> ~/.zshrc
+brew install docker && brew install docker-machine
+docker-machine create --driver virtualbox default
+docker-machine env default
+eval $(docker-machine env default)
+# and finally to test run:
+docker --version
+brew --version
